f0203ab2e8f68be5756ad74477e87d4cc8e46521
cadienlake <115520010+cadienlake@users.noreply.github.com>
2022-11-22 03:12:47 -0600

    Added 5-day Forecast functionality to the search. Everything is done on the API front. Storing and retrieving from localStorage still needs to be worked out.

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,7 @@ let currentCity = document.querySelector("#current-city");
 let currentTemp = document.querySelector("#current-temp");
 let currentWind = document.querySelector("#current-wind");
 let currentHumi = document.querySelector("#current-humi");
+let forecastCards = document.querySelector("#forecast-cards");
 let currentUrl = [];
 let fiveDayUrl = [];
 let today = dayjs();
@@ -23,17 +24,15 @@ function init() {
   //     searchedCities = storedCities;
   //   }
 }
-``;
-
 function search() {
   // assign variable to value of text box on HTML page
   let cityLogged = searchInput.value.trim();
-  let currentUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityLogged}&appid=517f19dc586407c39701b016a6edf914&units=imperial`;
+  let currentUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityLogged}&appid=0d0089143b24c1bb1408e815d3e61ea9&units=imperial`;
   if (cityLogged === "") {
     return;
   }
   searchInput.value = "";
-//   HOW DO I PUSH TO THE SEARCHEDCITIES ARRAY WITHOUT RESETTING IT IN THIS FUNCTION?
+  //   HOW DO I PUSH TO THE SEARCHEDCITIES ARRAY WITHOUT RESETTING IT IN THIS FUNCTION?
   localStorage.setItem("cities", cityLogged);
   console.log(cityLogged);
   fetch(currentUrl)
@@ -43,18 +42,48 @@ function search() {
     .then(function (data) {
       console.log(data);
       // build the card
-      // CHICAGO
+      // search for icon and coordinates in API
       let icon = data.weather[0].icon;
+      let lon = data.coord.lon;
+      let lat = data.coord.lat;
+      // Apply coordinates to the search for five day forecast (since One Call doesn't have a by city search smh)
+      let fiveDayUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&appid=0d0089143b24c1bb1408e815d3e61ea9&units=imperial`;
       console.log(icon);
-    //   WHY IS THE ICON ON A NEW LINE?
+      //   WHY IS THE ICON ON A NEW LINE?
       currentCity.innerHTML = `${data.name} ${today.format("M/DD/YYYY")} <img src="./assets/icons/${icon}.png" class="w-12">`;
       // temp, wind, and humidity
-      currentTemp.innerHTML = `Temp: ${data.main.temp}°F`;
+      currentTemp.innerHTML = `Temperature: ${data.main.temp}°F`;
       currentWind.innerHTML = `Wind: ${data.wind.speed}MPH`;
       currentHumi.innerHTML = `Humidity: ${data.main.humidity}%`;
-      // 5-DAY FORECAST
-      fetch;
+      fetch(fiveDayUrl)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+          let daily = data.daily;
+          for (let i = 1; i < 6; i++) {
+            let iconTwo = data.daily[i].weather[0].icon;
+            let divEl = document.createElement("div");
+            let dateEl = document.createElement("h3");
+            let tempEl = document.createElement("p");
+            let windEl = document.createElement("p");
+            let humiEl = document.createElement("p");
+            divEl.classList.add("border-2", "rounded", "border-black", "p-2", "text-white", "bg-sky-900");
+            dateEl.classList.add("text-lg", "font-bold", "pb-3");
+            tempEl.classList.add("pb-3");
+            windEl.classList.add("pb-3");
+            humiEl.classList.add("pb-3");
+            dateEl.innerHTML = `${today.add([i], "day").format("M/DD/YYYY")} <img src="./assets/icons/${iconTwo}.png" class="w-12">`;
+            tempEl.innerHTML = `Temp: ${daily[i].temp.day}°F`;
+            windEl.innerHTML = `Wind: ${daily[i].wind_speed}MPH`;
+            humiEl.innerHTML = `Humidity: ${daily[i].humidity}%`;
+            divEl.append(dateEl, tempEl, windEl, humiEl);
+            forecastCards.append(divEl);
+          }
+        });
     });
+  // 5-DAY FORECAST
 }
 
 // FUNCTION CALLS
@@ -64,8 +93,3 @@ init();
 // search button event listener
 searchBtn.addEventListener("click", search);
 // click on past search results (same as search button)
-
-// api.openweathermap.org/geo/1.0/direct?q=chicago&appid=517f19dc586407c39701b016a6edf914
-// api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=517f19dc586407c39701b016a6edf914
-
-// api.openweathermap.org/data/2.5/forecast?q=chicago&appid=517f19dc586407c39701b016a6edf914
